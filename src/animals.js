5d238d57b4730ca76a1c47664f6bff7c81c3c605
DanielDeanGithub <github-new@inboxrefresh.com>
2024-04-06 12:30:50 +0100

    Import animal images to animals.js

--- src/animals.js
+++ src/animals.js
@@ -1,14 +1,19 @@
+import dolphin from './images/dolphin.png';
+import lobster from './images/lobster.png';
+import starfish from './images/starfish.png';
+
+
 export const animals = {
     dolphin: {
-      image: '/images/dolphin.png',
+      image: dolphin,
       facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
     },
     lobster: {
-      image: '/images/lobster.png',
+      image: lobster,
       facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
     },
     starfish: {
-      image: '/images/starfish.png',
+      image: starfish,
       facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
     }
   };
