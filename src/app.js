2d2e7c08e925db016fb57c33eb332645c92e9f2d
DanielDeanGithub <github-new@inboxrefresh.com>
2024-04-06 12:11:11 +0100

    Update animalFacts const to include background

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import { animals } from './animals';
 
-const background = <img src='../images/ocean.jpg' className='background' alt='ocean' />
+const background = <img src='images/ocean.jpg' className='background' alt='ocean' />
 
 const title = '';
-const animalFacts = <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>;
+const animalFacts = (
+    <div>
+        {background}
+        <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
+    </div>
+);
 
 const App = () => {
     return (
